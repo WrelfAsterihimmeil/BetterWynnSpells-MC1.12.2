8ebbfa14811576a08c3b87d409dc4c6aad391776
WrelfAsterihimmeil <mugensoumi@gmail.com>
2022-02-16 19:52:22 +0900

    Reverted Ref#tickBetweenClicks

--- src/main/java/net/wrelf/betterwynnspell/Ref.java
+++ src/main/java/net/wrelf/betterwynnspell/Ref.java
@@ -1,6 +1,6 @@
 package net.wrelf.betterwynnspell;
 
 public class Ref {
-    public static int tickBetweenClicks = 20;
+    public static int tickBetweenClicks = 4;
     public static boolean isArcher = false;
 }
